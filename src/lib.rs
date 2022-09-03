5be412737980c2530bf56adb07aab9ceb18cd836
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-09-02 22:19:34 -1000

    render macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod components;
 pub mod error;
 pub mod event;
+pub mod macros;
 pub mod state;
 pub mod terminal;
---
+++ src/macros/render.rs
@@ -0,0 +1,45 @@
+#[macro_export]
+macro_rules! render {
+  // component with children
+  (
+    $component:ident($($prop:ident: $value:expr),* $(,)*) { $($children:tt)* }
+  ) => {
+    $component {
+      $($prop: $value.try_into().expect(&format!("try_info failed for argument '{}'", stringify!($prop))),)*
+
+      children: $crate::render! { @children [] $($children)* },
+
+      ..Default::default()
+    }.into()
+  };
+
+  // component without children
+  (
+    $component:ident($($prop:ident: $value:expr),* $(,)*)
+  ) => {
+    $component {
+      $($prop: $value.try_into().expect(&format!("try_info failed for argument '{}'", stringify!($prop))),)*
+
+      ..Default::default()
+    }.into()
+  };
+
+  // push-down accumulator for parsing children
+  (
+    @children [$($parsed:tt)*]
+  ) => {
+    [ $($parsed)* ]
+  };
+
+  (
+    @children [$($parsed:tt)*] $component:ident($($prop:ident: $value:expr),* $(,)*) { $($children:tt)* } $($rest:tt)*
+  ) => {
+    $crate::render! { @children [ $($parsed)* $crate::render! { $component($($prop: $value,)*) { $($children)* } }, ] $($rest)* }
+  };
+
+  (
+    @children [$($parsed:tt)*] $component:ident($($prop:ident: $value:expr),* ) $($rest:tt)*
+  ) => {
+    $crate::render! { @children [ $($parsed)* $crate::render! { $component($($prop: $value,)*) }, ] $($rest)* }
+  };
+}
