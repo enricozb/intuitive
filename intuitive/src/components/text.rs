86fdd324bd7c65b8b20a6668488ae2e79f11b4f1
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-09-11 13:52:32 -1000

    add multi-line support to Text

--- intuitive/src/components/text.rs
+++ intuitive/src/components/text.rs
@@ -4,7 +4,7 @@ use crate::{
   components::Component,
   element::{Any as AnyElement, Element},
   event::{KeyEvent, KeyHandler},
-  spans::Spans,
+  spans::{Span, Spans},
   terminal::{Frame, Rect},
 };
 
@@ -21,14 +21,40 @@ pub struct Text {
 impl Component for Text {
   fn render(&self) -> AnyElement {
     AnyElement::new(Frozen {
-      text: self.text.clone(),
+      text: self.text.clone().into(),
       on_key: self.on_key.clone(),
     })
   }
 }
 
+#[derive(Clone)]
+struct Lines(Vec<Spans>);
+
+impl From<Spans> for Lines {
+  fn from(spans: Spans) -> Self {
+    let mut expanded = Vec::new();
+
+    for span in spans {
+      let lines: Vec<&str> = span.text.split('\n').collect();
+      expanded.push(Some(Span::new(lines[0], span.style)));
+
+      for line in &lines[1..] {
+        expanded.push(None);
+        expanded.push(Some(Span::new(*line, span.style)));
+      }
+    }
+
+    let split = expanded
+      .split(|span| span.is_none())
+      .map(|spans| Spans::new(spans.into_iter().flatten().cloned().collect::<Vec<Span>>()))
+      .collect();
+
+    Lines(split)
+  }
+}
+
 struct Frozen {
-  text: Spans,
+  text: Lines,
   on_key: KeyHandler,
 }
 
@@ -38,7 +64,7 @@ impl Element for Frozen {
   }
 
   fn draw(&self, rect: Rect, frame: &mut Frame) {
-    let widget = Paragraph::new::<TuiSpans>((&self.text).into());
+    let widget = Paragraph::new::<Vec<TuiSpans>>(self.text.clone().0.into_iter().map(TuiSpans::from).collect());
 
     frame.render_widget(widget, rect);
   }
